898479545c8925a97514ba4c78d78e770124a7f6
jleon-1 <jefferson.leon@consulti.ec>
2022-02-25 08:44:01 -0500

    grafico

--- client/src/containers/panel/Panel.js
+++ client/src/containers/panel/Panel.js
@@ -19,7 +19,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { LocalizationTable, TableIcons, removeAccent } from '../../utils/table.js'
 import MaterialTable from "material-table";
-import { Grid, IconButton } from '@material-ui/core';
+import { FormControl, Grid, IconButton, InputLabel, MenuItem, Select, Slider } from '@material-ui/core';
 import { obtenerSistemaEvaluaciones, obtenerTodos } from '../../utils/API/sistemas.js';
 import Crear from './components/Crear'
 import Eliminar from './components/Eliminar'
@@ -27,12 +27,13 @@ import { obtenerTodos as obtenerMetricasSistemas } from '../../utils/API/cliente
 import Bar from './components/Bar';
 import BarVertical from './components/BarVertical';
 import PieChart from './components/PieChart';
-
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import { utcDate } from '../../utils/Date'
 
 import Box from '@material-ui/core/Box';
 import Tab2 from './components/Tab2';
 import { obtenerTodos as obtenerTodosS } from '../../utils/API/facturas';
+import { obtenerTodos as obtenerTodosTareas, obtenerUsuariosTarea } from '../../utils/API/tareas';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Radio from './components/Radio';
 import noValue from '../../assets/noValue.svg'
@@ -77,8 +78,8 @@ const useStyles = makeStyles((theme) => ({
         color: '#fff',
         overflow: 'hidden',
         position: 'relative',
-     
-       
+
+
     },
     card2: {
         backgroundColor: "#5e35b1",
@@ -103,7 +104,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Sistemas(props) {
     const initializer = React.useContext(Initializer);
     const classes = useStyles();
-    const [data3, setData3] = React.useState({completas:0,pendientes:0})
+    const [data3, setData3] = React.useState({ completas: 0, pendientes: 0 })
 
     const [data, setData] = React.useState(null)
     const [data0, setData0] = React.useState([])
@@ -120,24 +121,29 @@ export default function Sistemas(props) {
     const [value, setValue] = React.useState(0);
     const [sistemas, setSistemas] = React.useState([])
     const [sistema, setSistema] = React.useState('')
-    const [data1, setData1] = React.useState({ventas:[],cantidad:[],meses:[]})
+    const [data1, setData1] = React.useState({ ventas: [], cantidad: [], meses: [] })
     const [cajaAbierta, setCajaAbierta] = React.useState(null)
-    const [comprasVentas, setComprasVentas] = React.useState({ventas:[],compras:[]})
-    const [cajaVenta, setCajaVenta] = React.useState({factura:[],caja:[]})
-    const [estadoTareas, setEstadoTareas] = React.useState({completas:[],incompletas:[]})
+    const [comprasVentas, setComprasVentas] = React.useState({ ventas: [], compras: [] })
+    const [cajaVenta, setCajaVenta] = React.useState({ factura: [], caja: [] })
+    const [estadoTareas, setEstadoTareas] = React.useState({ completas: [], incompletas: [] })
 
-    const [empleados,setEmpleados] = React.useState([])
+    const [empleados, setEmpleados] = React.useState([])
+    const [asignados, setAsignados] = React.useState([])
+
+    const [tareas, setTareas] = React.useState([])
+    const [tarea, setTarea] = React.useState('')
+
     React.useEffect(() => {
         if (initializer.usuario != null) {
-            obtenerEstadoTareas({},setEstadoTareas, initializer)
-            obtenerUsuarioTareas({},setEmpleados, initializer)
-            obtenerTareasEstado({},setData3,initializer)
-       
+            obtenerEstadoTareas({}, setEstadoTareas, initializer)
+            obtenerUsuarioTareas({}, setEmpleados, initializer)
+            obtenerTareasEstado({}, setData3, initializer)
+            obtenerTodosTareas(setTareas, initializer)
         }
     }, [initializer.usuario])
     const getFirstLast = () => {
         return "(" + utcDate(desde) + " hasta " + utcDate(hasta) + ")"
-      }
+    }
     const getFirst = () => {
         let date = new Date();
         let primerDia = new Date(date.getFullYear(), date.getMonth(), 1);
@@ -174,38 +180,44 @@ export default function Sistemas(props) {
         })
         return object
     }
-    const filtrarFecha=(userId)=>{
-       
-        obtenerEstadoTareas({desde:utcDate(desde),hasta:utcDate(hasta),user_id:userId},setEstadoTareas, initializer)
-        obtenerUsuarioTareas({desde:utcDate(desde),hasta:utcDate(hasta),user_id:userId},setEmpleados, initializer)
-        setData3({completas:0,pendientes:0})
-        obtenerTareasEstado({desde:utcDate(desde),hasta:utcDate(hasta),user_id:userId},setData3,initializer)
+    const filtrarFecha = (userId) => {
+
+        obtenerEstadoTareas({ desde: utcDate(desde), hasta: utcDate(hasta), user_id: userId }, setEstadoTareas, initializer)
+        obtenerUsuarioTareas({ desde: utcDate(desde), hasta: utcDate(hasta), user_id: userId }, setEmpleados, initializer)
+        setData3({ completas: 0, pendientes: 0 })
+        obtenerTareasEstado({ desde: utcDate(desde), hasta: utcDate(hasta), user_id: userId }, setData3, initializer)
 
     }
-    const colorPuntaje=(val)=>{
-        if((val*100)<=54){
+    const colorPuntaje = (val) => {
+        if ((val * 100) <= 54) {
             return 'red';
-        }else if(val*100>=55&&val*100<=69){
+        } else if (val * 100 >= 55 && val * 100 <= 69) {
             return 'yellow';
-        }else if(val*100>=70&&val*100<=100){
+        } else if (val * 100 >= 70 && val * 100 <= 100) {
             return 'green';
         }
+    }
+    const obtenerValor = (id) => {
+
+
+        return id.percent
+
     }
     return (
         <Grid container spacing={2}>
             <Crear sistema={selected} setSelected={setSelected} setOpen={setOpen} open={open} carga={carga} />
             <Eliminar sistema={selected2} setOpen={setOpen2} open={open2} carga={carga} />
-            <Grid item xs={12} md={12} style={{display:'flex',justifyContent:'space-between'}}>
+            <Grid item xs={12} md={12} style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <Typography variant="h5" >
-                    Dashboard 
-                </Typography> 
-                <FiltroPanel desde={desde} hasta={hasta} setDesde={setDesde} setHasta={setHasta} filtrarFecha={filtrarFecha}/>
+                    Dashboard
+                </Typography>
+                <FiltroPanel desde={desde} hasta={hasta} setDesde={setDesde} setHasta={setHasta} filtrarFecha={filtrarFecha} />
 
             </Grid>
-            
+
             <Grid item xs={12} md={12} >
                 <Grid container spacing={2}>
-       
+
                     <Grid item xs={12} md={4}>
                         <Card class={classes.card} style={{ width: '100%', height: 157, marginRight: 20, marginBottom: 5, backgroundColor: '#5e35b1', borderRadius: 12 }}>
                             <CardContent>
@@ -256,17 +268,17 @@ export default function Sistemas(props) {
                             </CardContent>
                         </Card>
                     </Grid>
-                   
+
                 </Grid>
             </Grid>
-     
+
             <Grid item md={6} xs={12}>
                 <div style={{ marginTop: 15 }} >
-                
-                {
+
+                    {
                         data3.pendientes != 0 || data3.completas != 0 ? (
-                            <PieChart pendientes={data3.pendientes} completas={data3.completas} text="Tareas Pendientes/Realizadas"/>
-                            )
+                            <PieChart pendientes={data3.pendientes} completas={data3.completas} text="Tareas Pendientes/Realizadas" />
+                        )
                             :
                             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                                 <img src={noValue} width={150} height={150} alt="" srcset="" />
@@ -277,7 +289,7 @@ export default function Sistemas(props) {
 
 
             </Grid>
-     
+
             <Grid item md={6} xs={12}>
                 <MaterialTable
                     icons={TableIcons}
@@ -286,7 +298,7 @@ export default function Sistemas(props) {
                         { title: "Empleado", field: "user" },
 
                         {
-                            title: "Completas", field: "total", render: rowData => <span style={{fontWeight:'bold'}}>{rowData.total}</span>
+                            title: "Completas", field: "total", render: rowData => <span style={{ fontWeight: 'bold' }}>{rowData.total}</span>
                         },
 
 
@@ -297,7 +309,7 @@ export default function Sistemas(props) {
                         empleados
                     }
                     localization={LocalizationTable}
-                
+
                     title="Cumplimiento de tareas/Empleado"
                     options={{
 
@@ -318,6 +330,73 @@ export default function Sistemas(props) {
                     }}
 
                 />
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl variant="outlined" style={{width:'100%'}}>
+                    <InputLabel id="demo-simple-select-outlined-label">Seleccione una tarea</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-outlined-label"
+                        id="demo-simple-select-outlined"
+                        value={tarea}
+                        onChange={(e) => {
+                            setTarea(e.target.value)
+
+                            obtenerUsuariosTarea(e.target.value, setAsignados, initializer)
+
+                        }}
+
+                        label="Seleccione una tarea"
+                    >
+                        <MenuItem value="">
+                            <em>Seleccione una opción</em>
+                        </MenuItem>
+
+                        {
+                            tareas.map((e) => (
+                                <MenuItem value={e.id}>{e.name}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+
+                {
+                    asignados.map((e) => (
+                        <Grid container spacing={2} alignItems="center">
+                            <Grid item xs={12}>
+                                <span>{e.names + " " + e.last_names} ({e.percent}%)</span>
+                            </Grid>
+                            <Grid item>
+                                <CheckCircleOutlineIcon color="primary" />
+                            </Grid>
+                            <Grid item xs>
+                                <Slider
+                                    value={typeof e.percent === 'number' ? obtenerValor(e) : 0}
+
+
+                                    valueLabelDisplay="auto"
+                                    marks={
+                                        [
+                                            {
+                                                value: 0,
+                                                label: '0%',
+                                            },
+                                            {
+                                                value: 100,
+                                                label: '100%',
+                                            }]
+
+                                    }
+                                    aria-labelledby="input-slider"
+                                />
+                            </Grid>
+
+                        </Grid>
+
+                    ))
+                }
+
             </Grid>
 
         </Grid>
